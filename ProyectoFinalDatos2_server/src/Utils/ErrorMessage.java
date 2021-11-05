52ae1ef33249ea8d6141f514eb7a8033eaceaffb
luisda190519 <72144336+luisda190519@users.noreply.github.com>
2021-11-05 15:41:55 -0500

    a

---
+++ ProyectoFinalDatos2_server/src/Utils/ErrorMessage.java
@@ -0,0 +1,10 @@
+package Utils;
+
+import javax.swing.JOptionPane;
+
+public class ErrorMessage {
+
+    public static void main(String args[]) {
+        JOptionPane.showMessageDialog(new JOptionPane(), "This is a library, not a program.\nPlease run VoiceChat_Server or VoiceChat_Client");
+    }
+}
---
+++ ProyectoFinalDatos2_server/src/Utils/Utils.java
@@ -0,0 +1,36 @@
+package Utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class Utils {
+
+    public static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    public static String getExternalIP() {
+        try {
+            URL myIp = new URL("http://checkip.dyndns.org/");
+            BufferedReader in = new BufferedReader(new InputStreamReader(myIp.openStream()));
+            String s = in.readLine();
+            return s.substring(s.lastIndexOf(":") + 2, s.lastIndexOf("</body>"));
+        } catch (Exception ex) {
+            return "error " + ex;
+        }
+    }
+
+    public static String getInternalIP() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            return "error";
+        }
+    }
+}
